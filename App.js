3269c6841aea6b1e2660e9bd3acfa5771311b5aa
gavin dhaliwal <gavin@pop-os.localdomain>
2021-02-18 19:33:07 +0000

    view for download youtube url

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 
 import DownloadScreen from "./screens/DownloadScreen";
 import LibaryScreen from "./screens/LibaryScreen";
@@ -11,19 +13,38 @@ import MySongsScreen from './screens/MySongsScreen';
 
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+function LibaryStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Libary" component={LibaryScreen} />
+      <Stack.Screen name="Download Music" component={DownloadScreen} />
+      <Stack.Screen name="My Playlists" component={MyPlaylistScreen} />
+      <Stack.Screen name="My Songs" component={MySongsScreen} />
+    </Stack.Navigator>
+  );
+}
+
 
 
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      {/* <Stack.Navigator>
         <Stack.Screen name="Libary" component={LibaryScreen} />
         <Stack.Screen name="Download Music" component={DownloadScreen} />
         <Stack.Screen name="My Playlists" component={MyPlaylistScreen} />
         <Stack.Screen name="My Songs" component={MySongsScreen} />
         <Stack.Screen name="Current Playlist" component={CurrentPlaylistScreen} />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+      <Tab.Navigator>
+        <Tab.Screen name="Download" component={DownloadScreen} />
+        <Tab.Screen name="Libary" component={LibaryStack} />
+        <Tab.Screen name="My Playlist" component={MyPlaylistScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
--- screens/DownloadScreen.js
+++ screens/DownloadScreen.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet } from "react-native";
+import { Container, Item, Input, Label, Button, Text } from 'native-base';
+import { Col, Row, Grid } from "react-native-easy-grid";
+
+
 
 const DownloadScreen = () => {
+
+    const [textValue, setTextValue] = useState('');
+
+    console.log(textValue);
+
     return (
-        <View style={styles.container}>
-            <Text>Download Screen</Text>
-        </View>
+        <Container>
+            <Grid>
+                <Row size={1} style={{ backgroundColor: '#F7FAFF', alignItems: 'center' }}>
+                    <Item stackedLabel style={{ marginLeft: 10, width: '100%', height: '10%', paddingRight: 25, borderColor: 'transparent' }}>
+                        <Label>Youtube URL</Label>
+                        <Item style={{ borderColor: 'transparent' }}>
+                            <Input placeholder="https://youtube...." onChangeText={(text) => setTextValue(text)} style={{ borderBottomWidth: 1, borderColor: '#595959', flex: 2, marginRight: 10 }} />
+                            <Button primary style={{ flex: 1 }}><Text>Download</Text></Button>
+                        </Item>
+                    </Item>
+                </Row>
+            </Grid>
+        </Container>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        justifyContent: "center"
-    }
 });
 
 export default DownloadScreen;
